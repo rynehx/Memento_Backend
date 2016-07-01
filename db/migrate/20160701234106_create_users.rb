0a965e282f1065430bd1fcf6b0f5821935ec5059
Henry <rynehx@gmail.com>
2016-07-01 16:47:37 -0700

    initial model and controller

---
+++ db/migrate/20160701234106_create_users.rb
@@ -0,0 +1,12 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+        t.string :username, null: false, index: true
+        t.string :session_token, null: false
+        t.string :pinterest_auth_code
+        t.string :pinterest_access_token, null: false
+        t.timestamps null: false
+    end
+     add_index :users, :username, unique: true
+  end
+end
